863f5f85957e6791f684dc440c0b95a33221ec97
Vince Cedie Pino <vincecediepino@gmail.com>
2023-12-03 14:03:34 +0700

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+  let result = '';
 
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < num; i ++) {
+    result += word;
+  }
+
+  return result;
 };
+
+console.log(repeatString("hey", 0));
 
 // Do not edit below this line
 module.exports = repeatString;
