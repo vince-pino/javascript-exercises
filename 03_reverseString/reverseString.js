7e3d1d0be1d332f411036cc6577fa5ea97759428
Vince Cedie Pino <vincecediepino@gmail.com>
2023-12-03 14:15:40 +0700

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let reverseWord = "";
+  for (let i = word.length-1 ; i >= 0; i--) {
+    reverseWord += word[i];
+  }
+  console.log(word.length);
+  return reverseWord;
 };
 
 // Do not edit below this line
