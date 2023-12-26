0e6e6e3da082fb3120b6e52357739ef5f1c26e39
Vince Cedie Pino <vincecediepino@gmail.com>
2023-12-26 15:10:22 +0700

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length === 0) return 0;
+  if (arr. length === 1) return arr[0];
+  if (arr.length > 1) {
+    let sum = 0;
+    console.log(sum);
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let product = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    product *= arr[i];
+  }
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, pow) {
+	return x ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0) return 1;
+  if (x === 1) return 1;
+  if (x > 1) {
+    let factorial = 1;
+    for (let i = 1; i <= x; i++) {
+      factorial *= i;
+    }
+    return factorial;
+  }
 };
 
 // Do not edit below this line
