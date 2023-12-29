01b36b8d831b4be66d342f78e500d4b37525f243
Vince Cedie Pino <vincecediepino@gmail.com>
2023-12-29 15:46:36 +0700

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  return  arr.reduce((oldest, currentPerson) => {
+    let oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    let currentAge = getAge(currentPerson.yearOfDeath,currentPerson.yearOfBirth);
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
+
+function getAge(death, birth) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
