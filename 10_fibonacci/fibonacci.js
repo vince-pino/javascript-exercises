18ac76b961865656142140e8e1867fcfcd679a1d
Vince Cedie Pino <vincecediepino@gmail.com>
2023-12-29 14:26:45 +0700

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    if (i === 1 || i === 2) {
+      arr.push(1);
+    }
+    else {
+      arr.push(arr[i-2] + arr[i-3])
+    }
+  }
 
+  return arr[arr.length-1];
 };
 
 // Do not edit below this line
